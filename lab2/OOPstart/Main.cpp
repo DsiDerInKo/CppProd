9ff882263d734c8d280fdd41f7001166cca45a6a
DsiDerInKo <nikbush8@gmail.com>
2023-02-28 01:51:20 +0300

    さようならございます。ぎんこうづせ

--- lab2/OOPstart/Main.cpp
+++ lab2/OOPstart/Main.cpp
@@ -4,26 +4,28 @@
 
 using namespace std;
 
-int main() {
-	int num;
-	Polynom* tmp = new Polynom();
-	while (true) {
-		
-		cout << '\n'<< "Choose comand: (Write value 1-11)" << '\n';
-		cout << "1: Create Polynom without your a,b,c" << '\n';
-		cout << "2: Create Polynom with your a,b,c" << '\n';
-		cout << "3: Number of roots" << '\n';
-		cout << "4: Get roots" << '\n';
-		cout << "5: Get max" << '\n';
-		cout << "6: Get min" << '\n';
-		cout << "7: Get value by X" << '\n';
-		cout << "8: print Polynom" << '\n';
-		cout << "9: view A cof" << '\n';
-		cout << "10: view B cof" << '\n';
-		cout << "11: view C cof" << '\n' << '\n';
-		
-		cin >> num;
-		switch (num){
+class Menu {
+public:
+	void start() {
+		int num;
+		Polynom* tmp = new Polynom();
+		while (true) {
+
+			cout << '\n' << "Choose comand: (Write value 1-11)" << '\n';
+			cout << "1: Create Polynom without your a,b,c" << '\n';
+			cout << "2: Create Polynom with your a,b,c" << '\n';
+			cout << "3: Number of roots" << '\n';
+			cout << "4: Get roots" << '\n';
+			cout << "5: Get max" << '\n';
+			cout << "6: Get min" << '\n';
+			cout << "7: Get value by X" << '\n';
+			cout << "8: print Polynom" << '\n';
+			cout << "9: view A cof" << '\n';
+			cout << "10: view B cof" << '\n';
+			cout << "11: view C cof" << '\n' << '\n';
+
+			cin >> num;
+			switch (num) {
 			case 1:
 				tmp->setACof(1);
 				tmp->setBCof(1);
@@ -36,15 +38,15 @@ int main() {
 				tmp->setACof(a);
 				tmp->setBCof(b);
 				tmp->setCCof(c);
-				
+
 				break;
 			case 3:
 				cout << tmp->getNumberOfRoots() << '\n';
 				break;
 			case 4:
 				array<double, 2> newtmp = tmp->getRoots();
-				if (newtmp[0]==NULL) cout << "No roots" << '\n';
-				else if (newtmp[1]==NULL) cout << newtmp[0] << '\n';
+				if (newtmp[0] == NULL) cout << "No roots" << '\n';
+				else if (newtmp[1] == NULL) cout << newtmp[0] << '\n';
 				else cout << newtmp[0] << " and " << newtmp[1] << '\n';
 				break;
 			case 5:
@@ -71,13 +73,22 @@ int main() {
 			case 11:
 				cout << tmp->getCCof() << '\n';
 				break;
-		default:
-			cout << "Invalid comand. Write value 1-11" << '\n';
-			
-			break;
+			default:
+				cout << "Invalid comand. Write value 1-11" << '\n';
+
+				break;
+			}
+
 		}
 
 	}
+
+};
+
+int main() {
+	
+	Menu* self = new Menu();
+	self->start();
 
 	return 0;
 }
